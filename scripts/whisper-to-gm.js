f5a32407b88c64f389e79e43b63e5900a5b702d2
David Townshend <aquavitae69@gmail.com>
2024-02-24 08:13:34 +0200

    Add a new /t command, to whisper to targetted tokens

--- scripts/whisper-to-gm.js
+++ scripts/whisper-to-gm.js
@@ -1,17 +1,59 @@
+const getTokenOwner = (token) =>
+  Object.keys(token.actor.ownership)
+    .filter((id) => token.actor.ownership[id] === 3)
+    .map((id) => game.users.get(id).name);
+
 Hooks.on('chatCommandsReady', (commands) => {
   const cmd = commands.commands.get('/whisper');
   if (cmd) {
     cmd.description = 'Send a private message to another player. This will also be visible to the GM.';
-    cmd.callback = (_, parameters) => {
-      const m = parameters.match(/^(?:\[(.*?)\])(.*)/) || parameters.match(/^(.*?) (.*)/);
-      if (m && !m[1].match(/\bGM\b/)) {
-        const users = `GM, ${m[1]}`;
-        const message = m[2];
-        ui.chat.processMessage(`/whisper [${users}] ${message}`);
+    cmd.callback = (_, parameters, metadata) => {
+      if (metadata.speaker.alias === 'Gamemaster') {
+        return null;
+      }
+
+      const m = parameters.match(/^(?:\[(.*?)\])(.*)/) || p.match(/^(.*?) (.*)/);
+      if (!m) {
         return {};
       }
-      return null;
+
+      const targets = m[1];
+      const message = m[2];
+
+      if (targets.match(/\b(gamemaster|gm)\b/i)) {
+        return null;
+      }
+
+      ui.chat.processMessage(`/whisper [Gamemaster, ${targets.join(', ')}] ${message}`);
+
+      return {};
     };
     commands.register(cmd);
   }
+});
+
+Hooks.on('chatCommandsReady', (commands) => {
+  commands.register({
+    name: '/t',
+    module: 'aardvarks',
+    aliases: ['%'],
+    description: 'Whisper to the targeted tokens. This will also be visible to the GM',
+    icon: "<i class='fas fa-message'></i>",
+    requiredRole: 'NONE',
+    callback: (_, message) => {
+      const whisperTargets = Array.from(game.user.targets).flatMap((x) => getTokenOwner(x)).filter(x=>x !== 'Gamemaster');
+
+      const targetNames = Array.from(game.user.targets)
+        .map((x) => x.name)
+        .sort()
+        .join(', ');
+
+      const targets = Array.from(new Set([...whisperTargets])).join(', ');
+
+      const m = `<small><strong>To: <em>${targetNames}</em></strong></small><br/>${message}`;
+      ui.chat.processMessage(`/whisper [${targets}] ${m}`);
+
+      return {};
+    },
+  });
 });
